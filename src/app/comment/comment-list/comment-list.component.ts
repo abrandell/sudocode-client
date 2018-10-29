4af8cd8f1da1d065e7185d0ced127f4850ca4588
adbrand <37308440+abrandell@users.noreply.github.com>
2018-10-28 21:24:30 -0700

    reorganized structure

---
+++ src/app/comment/comment-list/comment-list.component.ts
@@ -0,0 +1,90 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {CommentPage} from '../../shared/comment-page';
+import {SortOrder} from '../../shared/sort-order';
+import {ProjectService} from '../../shared/project.service';
+import {ActivatedRoute} from '@angular/router';
+import {animate, query, stagger, style, transition, trigger} from '@angular/animations';
+
+@Component({
+  selector: 'app-comment-list',
+  templateUrl: './comment-list.component.html',
+  styleUrls: ['./comment-list.component.scss'],
+  animations: [
+    trigger('listStagger', [
+      transition('* <=> *', [
+        query(
+          ':enter',
+          [
+            style({opacity: 0, transform: 'translateY(-15px)'}),
+            stagger(
+              '50ms',
+              animate(
+                '550ms ease-out',
+                style({opacity: 1, transform: 'translateY(0px)'})
+              )
+            )
+          ],
+          {optional: true}
+        ),
+      ])
+    ])
+  ]
+})
+export class CommentListComponent implements OnInit {
+
+  protected comments: CommentPage;
+  protected pageNum: number;
+  protected projectId: number;
+  private order: SortOrder;
+
+  constructor(private projectService: ProjectService, private route: ActivatedRoute) {
+    this.route.params.subscribe(
+      params => this.projectId = params.id,
+      err => console.log(err)
+    );
+  }
+
+  ngOnInit() {
+    this.pageNum = 0;
+    this.sortByNewest();
+  }
+
+  protected sortByOldest(): void {
+    this.order = SortOrder.ASC;
+    this.fetchComments();
+  }
+
+  protected sortByNewest(): void {
+    this.order = SortOrder.DESC;
+    this.fetchComments();
+  }
+
+
+  protected nextCommentPage(): void {
+    if (!this.comments.last) {
+      this.pageNum++;
+      this.fetchComments();
+    }
+  }
+
+  protected prevCommentPage(): void {
+    if (!this.comments.first) {
+      this.pageNum--;
+      this.fetchComments();
+    }
+  }
+
+  private fetchComments(): void {
+    this.projectService.fetchProjectComments(this.projectId, this.order, this.pageNum)
+        .subscribe(
+          data => this.comments = data,
+          err => console.log(err)
+        );
+  }
+
+
+  public refreshCommentList(): void {
+    this.sortByNewest();
+  }
+
+}
---
+++ src/app/project/project-edit/project-edit.component.ts
@@ -0,0 +1,53 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {IProject} from '../IProject';
+import {ProjectService} from '../../shared/project.service';
+import {ProjectDetailComponent} from '../project-detail/project-detail.component';
+import {ProjectCreation} from '../project-search/project-creation';
+
+@Component({
+  selector: 'app-project-edit',
+  templateUrl: './project-edit.component.html',
+  styleUrls: ['./project-edit.component.scss']
+})
+export class ProjectEditComponent implements OnInit {
+
+  @Input() project: IProject;
+  editing = true;
+
+  difficulty = [
+    'basic',
+    'beginner',
+    'intermediate',
+    'advanced',
+    'expert'
+  ];
+
+
+  constructor(private service: ProjectService, public projectDetail: ProjectDetailComponent) {
+  }
+
+  ngOnInit() {
+
+
+  }
+
+  editProject() {
+    return this.service.updateProject(this.project)
+               .subscribe(
+                 status => console.log(status),
+                 err => console.log(err),
+                 () => this.projectDetail.ngOnInit()
+               );
+  }
+
+  cancel() {
+    this.projectDetail.editing = false;
+  }
+
+  get diagnostic() {
+    return JSON.stringify(this.project);
+  }
+
+
+
+}
